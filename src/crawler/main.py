ff0ffd72690586d7645da406638d82766ecb89bc
xiaocao <wdjoys@hotmail.com>
2023-01-09 17:56:15 +0800

    20230109

--- src/crawler/main.py
+++ src/crawler/main.py
@@ -2,7 +2,7 @@
 # @Author: xiaocao
 # @Date:   2023-01-07 14:26:05
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-01-08 23:32:01
+# @Last Modified time: 2023-01-09 17:56:06
 
 
 import re
@@ -55,8 +55,11 @@ def resolver(html, publish_source: PublishSource):
     for record in search_result:
         timestamp = time_format(
             year, record[publish_source.located_time], publish_source)
+
+        # TODO 去除已经过时的广告记录
+
         yield {
-            'timestamp': time_format(year, record[publish_source.located_time], publish_source),
+            'timestamp': timestamp,
             'url': record[publish_source.located_url],
             'name': record[publish_source.located_name],
             "ip": record[publish_source.located_ip],
@@ -90,20 +93,51 @@ def time_format(year: int, time_string: str, publish_source: PublishSource):
         raise ValueError
 
 
-def remove_duplicates_for_db(source):
-    """与数据库对比，去除数据库内已经存在的记录
+def get_records_from_db(datetime):
+    """查询尚未到展示时间的广告
 
     Args:
-        source (_type_): _description_
+        datetime (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
+    result = Servers.select(Servers.name, Servers.timestamp).where(
+        Servers.timestamp > datetime)
+
+    return result
+
+
+def remove_duplicates_for_db(records_db: List[Servers], records_crawler):
+    """ 1.与数据库对比，去除数据库内已经存在的记录
+        2.去除爬取的重复广告，并标记广告重复次数
+
+    Args:
+        records_db (_type_): _description_
 
     Returns:
         _type_: _description_
     """
 
-    return list(set(source))
+    tags_db = (f"{record.name}-{record.timestamp}" for record in records_db)
+
+    count_dict = {}
+
+    removed_duplicates_records = []
+
+    for record in records_crawler:
+        if tag := f"{record['name']}-{record['timestamp']}" not in tags_db:
+
+            if tag in count_dict:
+                count_dict[tag] += 1
+            else:
+                count_dict[tag] = 1
+                removed_duplicates_records.append(record)
+
+    return removed_duplicates_records, count_dict  # 去重的广告记录，广告
 
 
-def marks_counters_for_record(source):
+def marks_counters_for_record(records):
     """标记广告条数
 
     Args:
@@ -113,8 +147,17 @@ def marks_counters_for_record(source):
         _type_: _description_
     """
 
-    for record in source:
-        sorted()
+    count_dict = {}
+    removed_duplicates_records = []
+
+    for record in records:
+        if item := f"{record['url']}-{record['timestamp']}" in count_dict:
+            count_dict[item] += 1
+        else:
+            count_dict[item] = 1
+            removed_duplicates_records.append(record)
+
+    return removed_duplicates_records, count_dict  # 去重的广告记录，广告
 
 
 def load_publish_source_config() -> List[PublishSource]:
