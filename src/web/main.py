60f5cca5850321d26cff459df1d4b94389d8be42
xiaocao <wdjoys@hotmail.com>
2023-01-11 17:54:23 +0800

    host 0.0.0.0

--- src/web/main.py
+++ src/web/main.py
@@ -2,7 +2,7 @@
 # @Author: xiaocao
 # @Date:   2023-01-07 14:16:19
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-01-11 15:51:26
+# @Last Modified time: 2023-01-11 17:54:17
 
 from fastapi import FastAPI
 from web.views.data import router as data_router
@@ -15,7 +15,7 @@ app.include_router(data_router)
 
 def run():
     import uvicorn
-    uvicorn.run("web.main:app", port=9090)
+    uvicorn.run("web.main:app", host="0.0.0.0", port=9090)
 
 
 if __name__ == "__main__":
