d5e9c43f217851f7f87734cb2795dc2253b43e2a
xiaocao <wdjoys@hotmail.com>
2023-02-07 11:27:40 +0800

    开启Gzip压缩 level=2

--- src/web/main.py
+++ src/web/main.py
@@ -2,21 +2,23 @@
 # @Author: xiaocao
 # @Date:   2023-01-07 14:16:19
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-01-17 10:18:47
+# @Last Modified time: 2023-02-07 11:26:41
 
 from fastapi import FastAPI
 from web.views.data import router as data_router
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.gzip import GZipMiddleware
 
 app = FastAPI()
 
 app.include_router(data_router)
 
+# 开启Gzip压缩 level=2
+app.add_middleware(GZipMiddleware, minimum_size=1000, compresslevel=3)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=['*'],
-
+    allow_origins=["*"],
     allow_methods=["*"],
     allow_headers=["*"],
 )
@@ -24,6 +26,7 @@ app.add_middleware(
 
 def run():
     import uvicorn
+
     uvicorn.run("web.main:app", host="0.0.0.0", port=9090)
 
 
