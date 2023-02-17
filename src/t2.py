4f82fc6889e9bf47a647bd0e884fdf0143170abd
xiaocao <wdjoys@hotmail.com>
2023-02-17 17:10:29 +0800

    优化sql url 字段长度

--- src/t2.py
+++ src/t2.py
@@ -2,27 +2,14 @@
 # @Author: xiaocao
 # @Date:   2023-01-12 15:31:11
 # @Last Modified by:   xiaocao
-# @Last Modified time: 2023-01-12 16:42:47
+# @Last Modified time: 2023-02-16 16:03:46
 
 
-# the result string and convert the values to python ints.
+def isPalindrome(x: int) -> bool:
+    if x < 0:
+        return False
+    y = int("".join([f"{x}"[i] for i in range(f"{x}".__len__() - 1, -1, -1)]))
+    return x == y
 
 
-from db.db import ServersAdCount, ServersAd
-if __name__ == "__main__":
-
-    from peewee import fn
-
-    def convert_ids(s): return [int(i) for i in (s or '').split(',') if i]
-
-    ids = (fn
-           .GROUP_CONCAT(ServersAdCount.source)
-           .python_value(convert_ids))
-
-    result = ServersAdCount.select(ServersAd.id.alias("ssss"), ServersAd.url, ServersAd.timestamp, ids.alias(
-        "ids")).join(ServersAd, on=(ServersAdCount.game == ServersAd.id)).group_by(ServersAdCount.game).where(
-        ServersAd.timestamp > 0)
-
-    for user in result:
-        print(user.serversad.timestamp, user.ids,
-              user.serversad.url, user.ssss)
+print(isPalindrome(-10))
